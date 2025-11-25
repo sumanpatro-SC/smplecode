c28435dcbf2dab0d31e4d66828ab5275b1b6de3c
sumanpatro-SC <suman.04patro@gmail.com>
2025-11-25 11:26:36 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
-class MyHnadler(SimpleHTTPRequestHandler)
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-def do_GET(self):
-    it self.path =='/':
-      self.path = 'index.html'
-    return supur().do_GET()
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'index.html'
+        return super().do_GET()
 
-if __name__ == "__main__":
-    erver = HTTPServer(('localhoust',8000)MyHhandler):
-    print("serving on https://localhoust:8000")
-    server.server_forever()
-    
+if __name__ == '__main__':
+    server = HTTPServer(('localhost', 8000), MyHandler)
+    print("Serving on http://localhost:8000")
+    server.serve_forever()
