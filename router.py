8f21c280653f59a7f52298cac2144bc0ea81fa2d
sumanpatro-SC <suman.04patro@gmail.com>
2025-11-26 10:12:13 +0000

    add database, student and student_handeler and utile

--- router.py
+++ router.py
@@ -1,16 +1,21 @@
+from http.server import BaseHTTPRequestHandler
+
 class StudentRouter(BaseHTTPRequestHandler):
+
     def do_GET(self):
         path = urlparse(self.path).path
 
-        if path in ("/","index.html"):
-            return serve_html(self)
-        if path.startswith("/static/"):
-            return serve_static(self)
+        # if path in ("/","index.html"):
+        #     return serve_html(self)
+
+        # if path.startswith("/static/"):
+        #     return serve_static(self)
+        
         if path == "/api/students":
             return get_all_students(self)
 
-        if path.startswith("/api/students/"):
-            student_id = int(path.split("/")[-1])
-            return get_student(self, student_id)
+        # if path.startswith("/api/students/"):
+        #     student_id = int(path.split("/")[-1])
+        #     return get_student(self, student_id)
 
         return send_404(self)
