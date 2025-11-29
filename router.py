0c2e546312b38c7ae7aea114c895acf4e35c9426
sumanpatro-SC <suman.04patro@gmail.com>
2025-11-29 17:06:42 +0000

    bug fix in r and c operation

--- router.py
+++ router.py
@@ -1,21 +1,28 @@
 from http.server import BaseHTTPRequestHandler
+from urllib.parse import urlparse
 
 class StudentRouter(BaseHTTPRequestHandler):
 
     def do_GET(self):
         path = urlparse(self.path).path
 
-        # if path in ("/","index.html"):
+        # if path in ("/", "index.html"):
         #     return serve_html(self)
 
         # if path.startswith("/static/"):
         #     return serve_static(self)
-        
+
         if path == "/api/students":
             return get_all_students(self)
 
-        # if path.startswith("/api/students/"):
-        #     student_id = int(path.split("/")[-1])
-        #     return get_student(self, student_id)
+        # API: Get student by ID
+        if path.startswith("/api/students/"):
+            student_id = int(path.split("/")[-1])
+            return get_student(self, student_id)
 
+        return send_404(self)
+
+    def do_POST(self):
+        if self.path == "/api/students":
+            return create_student(self)
         return send_404(self)
--- services/student_service.py
+++ services/student_service.py
@@ -5,5 +5,10 @@ from database.queries import (
     db_get_all
 )
 
+def service_get_one(student_id):
+     return db_get_one(student_id)
+
+def service_create(data):
+     return db_create(data)
 def service_get_all():
     return db_get_all()
